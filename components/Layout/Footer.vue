772362aa2dfa3e22b5fab5b0e2495ceae59bbf40
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-02-05 21:52:06 -0500

    refactor(Public): :zap: Since year added

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -10,7 +10,7 @@
       class="text-center"
       cols="12"
     >
-      {{ new Date().getFullYear() }} — <strong>Hentaini | All rights reserved</strong>
+      2019 - {{ new Date().getFullYear() }} | <strong>Hentaini | All rights reserved</strong>
       <p class="text-caption grey--text darken-3 pb-0 mb-0">
         Non of the videos in this website are stored in our servers.
       </p>
