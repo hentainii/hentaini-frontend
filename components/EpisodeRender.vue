ff5a86fd68a587e266ec7b00d0c1b01f58d50eda
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-02-05 21:51:04 -0500

    fix(Public): :zap: Fix title in Episode Render

--- components/EpisodeRender.vue
+++ components/EpisodeRender.vue
@@ -282,6 +282,36 @@ export default {
       user_id: ''
     }
   },
+  head () {
+    return {
+      title: this.episode ? this.episode.serie.title : 'Hentaini',
+      meta: this.episode
+        ? [
+            { hid: 'language', name: 'language', content: 'es' },
+            { hid: 'Revisit-After', name: 'Revisit-After', content: '3 days' },
+            { hid: 'audience', name: 'audience', content: 'all' },
+            { hid: 'rating', name: 'rating', content: 'general' },
+            { hid: 'distribution', name: 'distribution', content: 'global' },
+            { hid: 'document-type', name: 'document-type', content: 'Public' },
+            { hid: 'MSSmartTagsPreventParsing', name: 'MSSmartTagsPreventParsing', content: 'true' },
+            { hid: 'robots', name: 'robots', content: 'all' },
+            { hid: 'robots', name: 'robots', content: 'all, index, follow' },
+            { hid: 'googlebot', name: 'googlebot', content: 'all, index, follow' },
+            { hid: 'yahoo-slurp', name: 'yahoo-slurp', content: 'all, index, follow' },
+            { hid: 'msnbot', name: 'msnbot', content: 'index, follow' },
+            { hid: 'googlebot-image', name: 'googlebot-image', content: 'all' },
+            { hid: 'title', name: 'title', content: 'Watch ' + this.episode.serie.title + ' episode ' + this.episode.episode_number + ' free online HD' },
+            { hid: 'description', name: 'description', content: 'Watch online ' + this.episode.serie.title + ' in best quality. I mean, its Hentaini, the best place to watch your favourite series' },
+            { hid: 'keywords', name: 'keywords', content: 'Watch online hentai, best HD archive of the best of japanese culture for the world, hentaini, ahegao, yuri, yaoi, tentacle, maid, siscon, brocon' },
+            { hid: 'og:title', property: 'og:title', content: this.episode.serie.title },
+            { hid: 'og:description', property: 'og:description', content: 'Its a Hentai site, what do you expect? a no-girlfriend-depression solution?' },
+            { hid: 'og:url', property: 'og:url', content: 'https://hentaini.com' },
+            { hid: 'og:image', property: 'og:image', content: 'https://hentaini.com/screenshot/' + this.episode.serie.background_coverUrl },
+            { hid: 'author', name: 'author', content: 'hentaini' }
+          ]
+        : []
+    }
+  },
   computed: {
     serieId () {
       return this.$route.params.serie
@@ -348,36 +378,6 @@ export default {
       } else {
         this.user_id = this.$store.state.auth.username
       }
-    }
-  },
-  head () {
-    return {
-      title: this.episode ? this.episode.serie.title : 'Hentaini',
-      meta: this.episode
-        ? [
-            { hid: 'language', name: 'language', content: 'es' },
-            { hid: 'Revisit-After', name: 'Revisit-After', content: '3 days' },
-            { hid: 'audience', name: 'audience', content: 'all' },
-            { hid: 'rating', name: 'rating', content: 'general' },
-            { hid: 'distribution', name: 'distribution', content: 'global' },
-            { hid: 'document-type', name: 'document-type', content: 'Public' },
-            { hid: 'MSSmartTagsPreventParsing', name: 'MSSmartTagsPreventParsing', content: 'true' },
-            { hid: 'robots', name: 'robots', content: 'all' },
-            { hid: 'robots', name: 'robots', content: 'all, index, follow' },
-            { hid: 'googlebot', name: 'googlebot', content: 'all, index, follow' },
-            { hid: 'yahoo-slurp', name: 'yahoo-slurp', content: 'all, index, follow' },
-            { hid: 'msnbot', name: 'msnbot', content: 'index, follow' },
-            { hid: 'googlebot-image', name: 'googlebot-image', content: 'all' },
-            { hid: 'title', name: 'title', content: 'Watch ' + this.episode.serie.title + ' episode ' + this.episode.episode_number + ' free online HD' },
-            { hid: 'description', name: 'description', content: 'Watch online ' + this.episode.serie.title + ' in best quality. I mean, its Hentaini, the best place to watch your favourite series' },
-            { hid: 'keywords', name: 'keywords', content: 'Watch online hentai, best HD archive of the best of japanese culture for the world, hentaini, ahegao, yuri, yaoi, tentacle, maid, siscon, brocon' },
-            { hid: 'og:title', property: 'og:title', content: this.episode.serie.title },
-            { hid: 'og:description', property: 'og:description', content: 'Its a Hentai site, what do you expect? a no-girlfriend-depression solution?' },
-            { hid: 'og:url', property: 'og:url', content: 'https://hentaini.com' },
-            { hid: 'og:image', property: 'og:image', content: 'https://hentaini.com/screenshot/' + this.episode.serie.background_coverUrl },
-            { hid: 'author', name: 'author', content: 'hentaini' }
-          ]
-        : []
     }
   }
 }
