3394f55222a908b16f809e9c12b9208793efae3b
Nikoxx99 <nikoxx9900@gmail.com>
2025-04-27 11:40:46 -0500

    Modulos iniciales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxtjs/i18n',
+    'nuxt-gtag',
+    '@pinia/nuxt'
+  ]
 })
