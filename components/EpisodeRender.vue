f7ed835a48e49a7eb4428163f5662066b56b51c7
Nikoxx99 <nikoxx9900@gmail.com>
2025-05-15 18:52:34 -0500

    refactor: Update player filtering logic in EpisodeRender component
    - Enhanced player filtering to exclude an additional player based on short name in the EpisodeRender.vue file.

--- components/EpisodeRender.vue
+++ components/EpisodeRender.vue
@@ -498,7 +498,7 @@ export default {
       return this.favorites.some(favorite => favorite.url === this.episode.serie.url)
     },
     filteredPlayers () {
-      return this.episode.players.filter(player => player.name !== 'SSB' && player.name !== 'Cloud' && player.name !== 'C' && player.name !== 'TERA')
+      return this.episode.players.filter(player => player.name !== 'SSB' && player.name !== 'Cloud' && player.name !== 'C' && player.name !== 'TERA' && player.short_name !== 'TR')
     },
     isInWatchLater () {
       return this.watchlaters.some(watchlater => watchlater.serie.url === this.serieId && watchlater.episode_number === parseInt(this.episodeNumber))
