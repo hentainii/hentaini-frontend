810c4dd654cf3564f86c96345a6d1d67189c3ea7
Nikoxx99 <nikoxx9900@gmail.com>
2025-08-20 09:37:07 -0500

    fix(EpisodeRender): remove muted attribute from video element
    The muted attribute was incorrectly set to false, which could cause autoplay issues in some browsers. Removing it allows for better control of video playback behavior.

--- components/EpisodeRender.vue
+++ components/EpisodeRender.vue
@@ -50,7 +50,6 @@
                 playsinline
                 webkit-playsinline
                 crossorigin="anonymous"
-                muted="false"
                 autoplay="false"
                 style="position:absolute;top:0;left:0;width:100%;height:100%;border:0"
                 class="rounded-lg"
