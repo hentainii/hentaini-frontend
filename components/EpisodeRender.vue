cff338f600dd82d162d4d3bd16461a34e134684c
Nikoxx99 <nikoxx9900@gmail.com>
2025-10-19 20:05:44 -0500

    refactor(ads): remove and reo

--- components/EpisodeRender.vue
+++ components/EpisodeRender.vue
@@ -233,16 +233,16 @@
         <!-- Anuncios -->
         <v-container>
           <v-row class="mt-3 justify-center">
-            <!-- <client-only>
+            <client-only>
               <UtilsVueScriptComponent script="<script data-cfasync=&quot;false&quot; async type=&quot;text/javascript&quot; src=&quot;//tg.sikagaylies.com/tTx3MZK0mbso/129706&quot;></script>" />
             </client-only>
-            <client-only>
+            <!-- <client-only>
               <div id="bg-ssp-10357" />
               <UtilsVueScriptComponent script="<script data-cfasync='false' src='https://hentaini.com/bg.js' type='text/javascript'></script>" />
             </client-only> -->
-            <client-only>
+            <!-- <client-only>
               <UtilsVueScriptComponent script='<script data-cfasync="false" async type="text/javascript" src="//tg.sikagaylies.com/tTx3MZK0mbso/129706"></script>' />
-            </client-only>
+            </client-only> -->
           </v-row>
         </v-container>
 
--- components/LatestEpisodes.vue
+++ components/LatestEpisodes.vue
@@ -3,18 +3,18 @@
 <template>
   <v-container class="latest-episodes-container" fluid>
     <v-row class="justify-center">
-      <!-- <client-only>
+      <client-only>
         <UtilsVueScriptComponent script='<script async data-cfasync="false" src="https://platform.pubadx.one/pubadx-ad.js" type="text/javascript"></script>' />
       </client-only>
-      <v-row class="mt-3 justify-center">
+      <!-- <v-row class="mt-3 justify-center">
         <client-only>
           <div id="bg-ssp-10357" />
           <UtilsVueScriptComponent script="<script data-cfasync='false' src='https://hentaini.com/bg.js' type='text/javascript'></script>" />
         </client-only>
       </v-row> -->
-      <client-only>
+      <!-- <client-only>
         <UtilsVueScriptComponent script='<script data-cfasync="false" async type="text/javascript" src="//tg.sikagaylies.com/tTx3MZK0mbso/129706"></script>' />
-      </client-only>
+      </client-only> -->
     </v-row>
 
     <div class="section-header">
